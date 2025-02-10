e88fba38595f6b1129e9e1c15e017369cd89c1b1
Prakasha <gprakasha234@gmail.com>
2025-02-10 12:14:09 +0530

    fix: UI fix spinWheel

--- src/components/spinWheel/SpinWheel.tsx
+++ src/components/spinWheel/SpinWheel.tsx
@@ -1,59 +1,144 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 import styled from "styled-components";
+import {
+	BiMoney,
+	BiSolidCoffee,
+	BiSolidCoinStack,
+	BiSolidExit,
+} from "react-icons/bi";
+
+type PrizeType =
+	| "₹100"
+	| "Free Coffee"
+	| "Bonus XP"
+	| "Try Again"
+	| "₹5"
+	| "Jackpot!";
+
+interface PrizeIconProps {
+	prize: PrizeType;
+	className: string;
+}
 
 const SpinWheelContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 3;
-    position: relative;
-    background-color: #f8f9fa;
-    border-radius: 50%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 3;
+	position: relative;
+	border-radius: 50%;
+	box-shadow: 0px 0px 30px #673ab7, 0px 0px 0px #9c27b0;
 `;
 
-const Arrow = styled.div`
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 0;
-    height: 0;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-bottom: 20px solid #FF9800;
-    transform: translate(-50%, -30%) rotate(180deg);
+const RewardSection = styled(motion.div)`
+	border: 10px solid #673ab7;
 `;
 
-const prizes = ["$10", "$20", "Free Coffee", "Bonus XP", "$50", "Try Again", "$5", "Jackpot!"];
+const MiddleDot = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 70px;
+	height: 70px;
+	background: var(--bs-gray-900);
+	border-radius: 50%;
+	transform: translate(-50%, -50%);
+	z-index: 100;
+`;
+
+const PrizeSection = styled.div`
+	box-shadow: inset 0px 0px 0px #bebebe, inset 0px 10px 10px #ffffff;
+`;
+
+const prizes: PrizeType[] = [
+	"₹100",
+	"Free Coffee",
+	"Bonus XP",
+	"Try Again",
+	"₹5",
+	"Jackpot!",
+];
 const numSegments = prizes.length;
 const segmentAngle = 360 / numSegments;
 
+const iconStyles = {
+	"Free Coffee": {
+		icon: <BiSolidCoffee />,
+		style: { fill: "blanchedalmond" },
+	},
+	"Bonus XP": {
+		icon: <BiSolidCoinStack />,
+		style: { fill: "goldenrod" },
+	},
+	"Try Again": {
+		icon: <BiSolidExit />,
+		style: { stroke: "red", fill: "red" },
+	},
+	"Jackpot!": {
+		icon: <BiMoney />,
+		style: { fill: "goldenrod" },
+	},
+	"₹100": {
+		icon: <BiMoney />,
+		style: { fill: "goldenrod" },
+	},
+	"₹5": {
+		icon: <BiMoney />,
+		style: { fill: "green" },
+	},
+};
+
+const PrizeIcon = ({ prize, className }: PrizeIconProps) => {
+	const { icon, style } = iconStyles[prize] || iconStyles["₹100"]; // Default to ₹100 icon
+
+	return (
+		<div className={className} style={style}>
+			{icon}
+		</div>
+	);
+};
+
 export default function SpinWheel() {
-  const [rotation, setRotation] = useState(0);
-  const [isSpinning, setIsSpinning] = useState(false);
-  const [selectedPrize, setSelectedPrize] = useState<string>();
+	const [rotation, setRotation] = useState(0);
+	const [isSpinning, setIsSpinning] = useState(false);
+	const [selectedPrize, setSelectedPrize] = useState<PrizeType>();
+	const [segmentWidth, setSegmentWidth] = useState(0);
 
-  const spinWheel = () => {
-    if (isSpinning) return;
-    setIsSpinning(true);
+	const spinWheel = () => {
+		if (isSpinning) return;
+		setIsSpinning(true);
+		setSelectedPrize(undefined);
 
-    const randomSpin = Math.floor(Math.random() * numSegments) * segmentAngle;
-    const totalRotation = rotation + (360 * 5) + randomSpin;
-    console.log(totalRotation, "totalRotation");
-    setRotation(totalRotation);
+		const randomSpin =
+			Math.floor(Math.random() * numSegments) * segmentAngle;
+		const totalRotation = rotation + 360 * 5 + randomSpin;
+		console.log(totalRotation, "totalRotation");
+		setRotation(totalRotation);
 
-    setTimeout(() => {
-      const winningIndex = (numSegments - Math.floor(randomSpin / segmentAngle)) % numSegments;
-      setSelectedPrize(prizes[winningIndex]);
-      setIsSpinning(false);
-    }, 4000);
-  };
+		setTimeout(() => {
+			const winningIndex =
+				(numSegments - Math.floor(randomSpin / segmentAngle)) %
+				numSegments;
+			setSelectedPrize(prizes[winningIndex]);
+			setIsSpinning(false);
+		}, 4000);
+	};
 
-  return (
-		<div className="d-flex flex-column align-items-center gap-3 mt-5">
+	useEffect(() => {
+		// Formula 1: radians = degrees * (π/180);
+		// Formula 1: SegmentWidth = 2*radiusOfWheel*sin(segmentAngle)/2);
+		const radius = 150;
+		const radians = segmentAngle * (Math.PI / 180);
+		const segmentWidth = 2 * radius * Math.sin(radians / 2);
+		setSegmentWidth(segmentWidth);
+	}, []);
+
+	return (
+		<div className="d-flex flex-column align-items-center gap-3 mt-5 bg-black">
 			<SpinWheelContainer>
-				<motion.div
-					className="position-relative rounded-circle border border-dark d-flex align-items-center justify-content-center overflow-hidden"
+				<RewardSection
+					className="position-relative rounded-circle d-flex align-items-center justify-content-center overflow-hidden"
 					animate={{ rotate: rotation }}
 					transition={{ duration: 4, ease: "easeOut" }}
 					style={{
@@ -62,36 +147,59 @@ export default function SpinWheel() {
 					}}
 				>
 					{prizes.map((prize, index) => (
-						<div
+						<PrizeSection
 							key={index}
-							className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center"
+							className="position-absolute d-flex align-items-center justify-content-center"
 							style={{
 								transform: `rotate(${
 									index * segmentAngle
-								}deg) translateY(-50%)`,
-								clipPath: "polygon(100% 0%, 50% 100%, 0% 0%)",
-								backgroundColor:
-									index % 2 === 0 ? "#f8f9fa" : "#e9ecef",
+								}deg) translateY(50%)`,
+								clipPath:
+									"polygon(50% 0%, -8% 100%, 100% 100%)",
+								height: "50%",
+								width: `${segmentWidth}px`,
+								zIndex: numSegments - index,
+								// dark shiny effect on every alternate segment purple, pink and blue for the prizes
+								background:
+									selectedPrize === prize
+										? "var(--bs-focus-ring-color)"
+										: index % 2 === 0
+										? "#7e57c2"
+										: "#f06292",
 							}}
 						>
 							<span className="small fw-bold text-white bg-dark p-1 rounded mt-5">
 								{prize}
 							</span>
-						</div>
+						</PrizeSection>
 					))}
-				</motion.div>
-                <Arrow />
+				</RewardSection>
+				<MiddleDot className="d-flex align-items-center justify-content-center">
+					{
+						// don't rotate Icon
+						selectedPrize && (
+							<PrizeIcon
+								key={selectedPrize}
+								className="fs-1"
+								prize={selectedPrize}
+							/>
+						)
+					}
+				</MiddleDot>
+				{/* <Arrow /> */}
 			</SpinWheelContainer>
 			<button
-				className="btn btn-primary"
+				className="btn btn-light"
 				onClick={spinWheel}
 				disabled={isSpinning}
 			>
 				{isSpinning ? "Spinning..." : "Spin the Wheel"}
 			</button>
 			{selectedPrize && (
-				<p className="fs-5 fw-semibold">You won: {selectedPrize}!</p>
+				<p className="fs-5 fw-semibold text-light">
+					You won: {selectedPrize}!
+				</p>
 			)}
 		</div>
-  );
+	);
 }
